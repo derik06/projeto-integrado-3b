34e53b8029cdf3d8a5c8c2c267addaa97c76cea8
Derik Oliveira <derikoliveira@vindula.com.br>
2019-09-20 19:37:23 -0300

    Inicio do projeto, tudo rodando default e configurado

---
+++ src/main/java/br/usjt/Projeto/Integrado/B/ProjetoIntegrado3BApplication.java
@@ -0,0 +1,13 @@
+package br.usjt.Projeto.Integrado.B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegrado3BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegrado3BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/usjt/Projeto/Integrado/B/ProjetoIntegrado3BApplicationTests.java
@@ -0,0 +1,16 @@
+package br.usjt.Projeto.Integrado.B;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoIntegrado3BApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
